83bc57f49efd2c3d37a9f2dc0e6f45bb42c5a091
giovanni-lumini <giovanni.lumini28@gmail.com>
2024-10-10 17:01:48 +0200

    js-partial

--- main.js
+++ main.js
@@ -20,13 +20,10 @@ Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali
 */
 
 //1-Visualizzare in pagina 5 numeri casuali. (FATTO)
-let num_random1 = document.getElementById("num_random1");
-let num_random2 = document.getElementById("num_random2");
-let num_random3 = document.getElementById("num_random3");
-let num_random4 = document.getElementById("num_random4");
-let num_random5 = document.getElementById("num_random5");
+
 let container_num = document.getElementById("container_num");
 let container_imput = document.getElementById("container_imput");
+let btn = document.getElementById("btn");
 
 let random_1 = Math.floor(Math.random() * 11);
 let random_2 = Math.floor(Math.random() * 11);
@@ -34,20 +31,41 @@ let random_3 = Math.floor(Math.random() * 11);
 let random_4 = Math.floor(Math.random() * 11);
 let random_5 = Math.floor(Math.random() * 11);
 
-num_random1.innerHTML = random_1;
-num_random2.innerHTML = random_2;
-num_random3.innerHTML = random_3;
-num_random4.innerHTML = random_4;
-num_random5.innerHTML = random_5;
+let array_random_numbers = [random_1, random_2, random_3, random_4, random_5];
+container_num.innerHTML = array_random_numbers;
+console.log(array_random_numbers);
+
 
 //2-Da lì parte un timer di 30 secondi. (FATTO)
-const clock = setTimeout (my_function, 3000);
-
+const clock = setTimeout(my_function, 3000);
 //3-Dopo 30 secondi:
 //3.1-i numeri scompaiono (FATTO)
-//3.2-appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
-function my_function (){
+function my_function() {
     container_num.style.display = "none";
-    container_imput.style.display = "block";
+    /* container_imput.style.display = "block"; */
+    container_imput.classList.remove("d-none");
+}
+
+//3.2-appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+btn.addEventListener("click",user_numbers);
+
+function user_numbers() {
+    let n1 = document.getElementById("n1");
+    n1_value = n1.value;
+    let n2 = document.getElementById("n2");
+    n2_value = n2.value;
+    let n3 = document.getElementById("n3");
+    n3_value = n3.value;
+    let n4 = document.getElementById("n4");
+    n4_value = n4.value;
+    let n5 = document.getElementById("n5");
+    n5_value = n5.value;
+    let array_user_numbers = [n1_value, n2_value, n3_value, n4_value, n5_value];
+    console.log(array_user_numbers);
 }
 //4-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+
+
+
